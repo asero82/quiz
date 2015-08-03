e893bb4d26060ebdfae5bda77c0d4da1efe21b19
= <=>
2015-08-03 12:10:41 -0300

    Multiples respuestas

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,9 @@ router.get('/', function(req, res, next) {
 	  res.render('index', { title: 'Bienvenido a Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 router.get('/author', function(req, res, next){
 	console.log("En author");
