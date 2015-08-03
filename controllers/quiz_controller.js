1c44c2161c8c0da294098d708c2a71bd72a84bb1
= <=>
2015-08-03 13:38:16 -0300

    Valida entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -19,13 +19,13 @@ exports.load = function(req, res, next, quizId){
 //GET /quizes
 exports.index = function(req, res){
     models.Quiz.findAll().then(function(quizes){
-        res.render('quizes/index.ejs', {quizes: quizes});
+        res.render('quizes/index.ejs', {quizes: quizes, errors: []});
     })
 }
 
 //GET /quizes/:id
 exports.show = function(req, res) {
-    res.render('quizes/show', {quiz: req.quiz});
+    res.render('quizes/show', {quiz: req.quiz, errors: []});
 };
 
 // GET /quisez/:id/answer
@@ -36,7 +36,13 @@ exports.answer = function (req, res) {
         resultado = 'Correcto';
     }
     
-    res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+    res.render(
+        'quizes/answer', 
+        {
+            quiz: req.quiz, 
+            respuesta: resultado, 
+            errors: []
+        });
 };
 
 // GET /quizes/new
@@ -45,16 +51,22 @@ exports.new = function(req, res){
         {pregunta: "Pregunta", respuesta: "Respuesta"}
     );
     
-    res.render('quizes/new', {quiz: quiz});
-}
+    res.render('quizes/new', {quiz: quiz, errors: []});
+};
 
 // POST /quizes/create
 exports.create = function(req, res){
-    console.log("Creando respuesta");
     var quiz = models.Quiz.build(req.body.quiz);
-    console.log(quiz);
-    // Guarda en DB los campos pregunta y respuesta de quiz
-    quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-        res.redirect('/quizes');
-    }) // Redirección HTTP (URL relativo) lista de preguntas
+        
+    quiz.validate().then(
+        function(err){
+            if (err){
+                res.render('quizes/new', {quiz: quiz, errors: err.errors});
+            }else{
+                quiz // Save: guarda en DB campos pregunta y respuesta de quiz
+                .save({fields: ["pregunta", "respuesta"]})
+                .then(function(){ res.redirect('/quizes')});
+            }   //res.redirect: Redicección HTTP a lista de pregunta
+        }
+    );
 };
