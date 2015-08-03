b53483d084bc80e374540861dd471127505fd933
= <=>
2015-08-03 17:01:44 -0300

    Agrega campo tema para el p2p obligatorio modulo 8.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -48,7 +48,7 @@ exports.answer = function (req, res) {
 // GET /quizes/new
 exports.new = function(req, res){
     var quiz = models.Quiz.build( //crea un objeto build
-        {pregunta: "Pregunta", respuesta: "Respuesta"}
+        {pregunta: "", respuesta: "", tema:"otro"}
     );
 
     res.render('quizes/new', {quiz: quiz, errors: []});
@@ -63,8 +63,9 @@ exports.create = function(req, res){
             if (err){
                 res.render('quizes/new', {quiz: quiz, errors: err.errors});
             }else{
+                console.log(quiz);
                 quiz // Save: guarda en DB campos pregunta y respuesta de quiz
-                .save({fields: ["pregunta", "respuesta"]})
+                .save({fields: ["pregunta", "respuesta", "tema"]})
                 .then(function(){ res.redirect('/quizes')});
             }   //res.redirect: Redicección HTTP a lista de pregunta
         }
@@ -81,6 +82,7 @@ exports.edit = function(req, res){
 exports.update = function(req, res){
     req.quiz.pregunta = req.body.quiz.pregunta;
     req.quiz.respuesta = req.body.quiz.respuesta;
+    req.quiz.tema = req.body.quiz.tema;
 
     req.quiz
     .validate()
@@ -90,7 +92,7 @@ exports.update = function(req, res){
                 res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
             }else{
                 req.quiz
-                .save({fields: ["pregunta", "respuesta"]})
+                .save({fields: ["pregunta", "respuesta", "tema"]})
                 .then(function(){
                     res.redirect('/quizes');
                 })
